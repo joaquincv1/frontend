d3d09f77b227bbf94c37f6a18ffdf8c856a21653
Joaquin Castillo <joaquinacv04@gmail.com>
2025-10-17 16:42:10 -0300

    agregando backend

--- angular-mongodb-app/src/app/services/user.service.ts
+++ angular-mongodb-app/src/app/services/user.service.ts
@@ -7,7 +7,7 @@ import { User, ApiResponse } from '../interfaces/user.interface';
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = 'https://backend-uib4.onrender.com/api';
 
   constructor(private http: HttpClient) { }
 
